e07036457e1f12dbfa82fb0612e0468c92b11a75
thinkphp <thinkphp@qq.com>
2024-08-22 16:02:16 +0800

    FieldTypeTransform类命名空间调整

--- tests/stubs/TestFieldJsonDTO.php
+++ tests/stubs/TestFieldJsonDTO.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace tests\stubs;
 
 use think\Model;
-use think\model\contracts\FieldTypeTransform;
+use think\model\contract\FieldTypeTransform;
 
 class TestFieldJsonDTO implements FieldTypeTransform, \JsonSerializable
 {
